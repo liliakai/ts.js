275164ae6b4f41795016ef456d7984f3d20dae0e
lilia <liliakai@gmail.com>
2014-03-07 23:27:33 -0800

    Refactor for DRYness, readability, namespacing

--- ts.js
+++ ts.js
@@ -1,39 +1,55 @@
+var Whisper = new function() {
+  this.PreKeyMessage = dcodeIO.ProtoBuf.
+                       loadProtoFile("./WhisperTextProtocol.proto").
+                       build("PreKeyWhisperMessage");
+
+  this.KeyPair = function() {
+    var mysecret = crypto.getRandomValues(new Uint8Array(32));
+    mysecret[0] &= 248;
+    mysecret[31] &= 127;
+    mysecret[31] |= 64;
+    this.sk = c255lbase32encode(mysecret);
+    this.pk = curve25519b32(this.sk);
+  };
+
+  this.KeyPair.prototype.to_a = function() {
+    var a = new Uint16Array(16);
+    a.set(c255lbase32decode(this.pk));
+    return a;
+  };
+}();
+
 window.onload = function() {
-  var WhisperTextProtocol = dcodeIO.ProtoBuf.loadProtoFile("./WhisperTextProtocol.proto");
-  var PreKeyWhisperMessage = WhisperTextProtocol.build("PreKeyWhisperMessage");
+
+  var identity_key = new Whisper.KeyPair();
+  document.getElementById('privatekey').innerHTML = identity_key.sk;
+  document.getElementById('publickey').innerHTML  = identity_key.pk;
 
   var registrationid = crypto.getRandomValues(new Uint8Array(32));
-  var mysecret = crypto.getRandomValues(new Uint8Array(32));
-
-  mysecret[0] &= 248;
-  mysecret[31] &= 127;
-  mysecret[31] |= 64;
-  var privkey = c255lbase32encode(mysecret);
-  var pubkey = curve25519b32(privkey);
-  document.getElementById('privatekey').innerHTML = privkey;
-  document.getElementById('publickey').innerHTML = pubkey;
 
   var BATCH_SIZE = 25;
   var prekeys = [];
   var prekey_protos = [];
   for (var i=0; i < BATCH_SIZE; i++) {
-    var prekey_priv = crypto.getRandomValues(new Uint8Array(32));
-    prekey_priv[0] &= 248;
-    prekey_priv[31] &= 127;
-    prekey_priv[31] |= 64;
-    var prekey_priv = c255lbase32encode(prekey_priv);
-    var prekey_pub = curve25519b32(prekey_priv);
+    var prekey = new Whisper.KeyPair();
 
-    prekeys[i] = {id: i, private_key: prekey_priv, public_key: prekey_pub};
-    document.getElementById('prekeys').innerText += (i != 0 ? "\n" : "") + "    [" + i + "]\n        pub: " + prekey_pub + "\n       priv: " + prekey_priv;
+    prekeys[i] = {
+      id:          i,
+      private_key: prekey.sk,
+      public_key:  prekey.pk
+    };
 
-    var baseKey = new Uint16Array(16);
-    baseKey.set(c255lbase32decode(prekey_pub));
+    document.getElementById('prekeys').innerText +=
+      "  [" + i + "]\n      pub: " + prekey.pk + "\n      priv: " + prekey.sk;
 
-    var identityKey = new Uint16Array(16);
-    identityKey.set(c255lbase32decode(pubkey));
-    prekey_protos[i] = new PreKeyWhisperMessage(registrationid, i, baseKey, identityKey);
-    document.getElementById('prekeys').innerText += "\n      " + "proto: " + prekey_protos[i].encode64() + "\n";
+    prekey_protos[i] = new Whisper.PreKeyMessage(
+      registrationid,
+      i,
+      prekey.to_a(),
+      identity_key.to_a()
+    );
 
+    document.getElementById('prekeys').innerText +=
+      "\n      " + "proto: " + prekey_protos[i].encode64() + "\n\n";
   }
 };
